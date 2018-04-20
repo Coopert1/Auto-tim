95177c606789b7c01a321b4c1b0b21ac72e6afaf
Coopert <kupchynskyi@gmail.com>
2018-04-21 00:43:25 +0300

    add backdrop

--- dev/js/main-menu.js
+++ dev/js/main-menu.js
@@ -1,10 +1,13 @@
 $(function(){
 	$('.menu-list').click(function(){
-		$(this).toggleClass('active');
+		$(this).toggleClass('open');
+		$('.backdrop').toggleClass('on');
+		$('.bars').toggleClass('active');
 		$(".main-menu ul li ul").animate(
 			{height: 'toggle'},
 			{duration:300},
 		);
+		// $('.backdrop').toggleClass('active');
 	})
 	$('.search-mobile').click(function(){
 		$(this).toggleClass('active_search');
