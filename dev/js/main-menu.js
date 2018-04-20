1876e12e28da129c93563a5c9a67f95b0448d027
00King00 <mr.1bilous@gmail.com>
2018-04-21 00:21:18 +0300

    add producut & slider

--- dev/js/main-menu.js
+++ dev/js/main-menu.js
@@ -13,4 +13,49 @@ $(function(){
 			// {duration:100},
 		);
 	})
+	
+	var product_slider_1=$('#product-slider-1').lightSlider({
+		item: 4,
+		controls:false,
+		slideMove:4,
+		galleryMargin: 20,
+
+//		responsive:[
+//			{
+//				breakpoint: 1200,
+//				settings:{
+//					item:4,
+//					slideMove:4,
+//				}
+//			},
+//		],
+	});
+	var product_slider_2=$('#product-slider-2').lightSlider({
+		item: 5,
+		controls:false,
+		slideMove:5,
+
+		responsive:[
+			{
+				breakpoint: 1200,
+				settings:{
+					item:4,
+					slideMove:4,
+				}
+			},
+			
+		],
+	});
+	$(".ls-prevSlide").click(function(){
+		product_slider_1.goToPrevSlide();
+	});
+	$(".ls-nextSlide").click(function(){
+		product_slider_1.goToNextSlide();
+	});
+	$(".ls-prevSlide2").click(function(){
+		product_slider_2.goToPrevSlide();
+	});
+	$(".ls-nextSlide2").click(function(){
+		product_slider_2.goToNextSlide();
+	});
 });
